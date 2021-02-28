9408c445ddaa0f7195d64cc47975ccd94c2b9e86
Aashish Sud <aashishsud1139066@hotmail.com>
2021-02-28 19:03:01 +0530

    Added base types for InvestmentComponent and DepositElements

---
+++ FinancialTracker/CommonTypes/BaseTypes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FinancialTracker.CommonTypes.BaseTypes;
+
+namespace FinancialTracker.CommonTypes
+{
+    class BaseTypes
+    {
+        public enum Period_E
+        {
+            Day,
+            Month,
+            Quarter,
+            BiAnnual,
+            Annual
+        }
+
+        public enum InvestmentType_E
+        {
+            SavingsAccount,
+            FixedDeposit,                           // DepositElements
+            RecurringDeposit,                       // DepositElements
+            RecurringDepositFlexi,
+            Stock,
+            MutualFunds,
+            ElectronicallyTradedFunds,
+            Bonds                                   // DepositElements
+        }
+
+        public enum Institute_E
+        {
+            ICICIBank,
+            ICICIDirect,
+            SBIBank
+        }
+    }
+
+    class RateOfInterest
+    {
+
+        /// <summary>
+        /// Rate of Interest
+        /// </summary>
+        public int Interest { get; set; }
+
+        /// <summary>
+        /// Period at which the rate of interest is calculated
+        /// </summary>
+        public Period_E Period_Type { get; set; }
+
+        /// <summary>
+        /// Recorded date for the ROI
+        /// </summary>
+        public DateTime Date_Of_Record { get; set; }
+    }
+}
---
+++ FinancialTracker/CommonTypes/InvestmentComponent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FinancialTracker.CommonTypes.BaseTypes;
+
+namespace FinancialTracker.CommonTypes
+{
+    class InvestmentComponent
+    {
+
+        /// <summary>
+        /// Name of investment
+        /// </summary>
+        public string Name { get; set; }
+
+
+        public InvestmentType_E Type { get; set; }
+
+
+        public DepositElements Deposit_Elements { get; set; }
+
+        public Institute_E Institute { get; set; }
+
+    }
+
+    class DepositElements
+    {
+
+        /// <summary>
+        /// Amount of initial/recurring investment
+        /// </summary>
+        public int Principal { get; set; }
+
+        /// <summary>
+        /// Period for the Deposit to mature
+        /// </summary>
+        public int Period { get; set; }
+
+        /// <summary>
+        /// Rate of interest for the Deposit
+        /// </summary>
+        public List<RateOfInterest> Rate_Of_Interest_List { get; set; }
+
+        /// <summary>
+        /// Is the Deposit Automatically renewed
+        /// </summary>
+        public bool Is_Auto_Renewed { get; set; }
+    }
+}
