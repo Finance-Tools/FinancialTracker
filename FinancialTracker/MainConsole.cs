9fa4dfc5fab1227bc46e3b7e204d668237eb4207
Aashish Sud <aashishsud1139066@hotmail.com>
2021-02-28 19:03:23 +0530

    Added a CLI interface for the App

---
+++ FinancialTracker/MainConsole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialTracker
+{
+    class MainConsole
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                Console.WriteLine("Hello From Console");
+            }
+            else
+            {
+                Console.WriteLine("Hello From GUI");
+                RunApplication();
+            }
+        }
+
+
+        private static void RunApplication()
+        {
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
